fb1d7305d71b63f9c4687f461ebe02116b96945e
Destinyrich <itsdestinyrich@gmail.com>
2023-03-16 01:43:19 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ /**
+  * main - prints "Programming is like building a multilingual 
+  * puzzle, followed by a newlin."
+  *  return 0.
+  */
+
+ int main(void)
+
+{
+ 
+  puts("/"Programming is like buiding a mutilingual puzzle);
+
+  return(0);
+	 
+ }
