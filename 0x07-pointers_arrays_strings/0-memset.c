9d549d48d99f44e187aabf90b482c11df00d905f
Destinyrich <itsdestinyrich@gmail.com>
2023-04-03 10:00:08 -0700

    done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,9 +7,10 @@
  *
  * Return: changed array with new value for n bytes
  */
-char *_memset(char *s, char b , unsigned int n )
+char *_memset(char *s, char b, unsigned int n)
 {
 	int a = 0;
+
 	for (; n > 0; a++)
 	{
 		s[a] = b;
