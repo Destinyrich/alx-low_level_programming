5d4924b1f0c2e6828ed709fd52b828a3b5d35816
Destinyrich <itsdestinyrich@gmail.com>
2023-04-03 11:28:22 -0700

    last

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int d;
+	int f;
+
+	for (d = 0; f < 8; d++)
+	{
+		for (f = 0; f < 8; f++)
+			_putchar(a[d][f]);
+		_putchar('\n');
+	}
+}
