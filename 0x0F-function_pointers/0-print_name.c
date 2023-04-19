b7c9887dfb1d5579364273e4fbf0f93b8bc7a51b
Destinyrich <itsdestinyrich@gmail.com>
2023-04-19 13:35:01 -0700

    first proj

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,10 @@
  * @name: string to add
  * @f: pointer to function
  * Return: nothing
- * */
+ **/
 void print_name(char *name, void(*f)(char *))
 {
-	if(name == NULL || f == NUL)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
