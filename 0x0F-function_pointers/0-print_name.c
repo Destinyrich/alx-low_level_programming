e52533fdc16fdf4f02e2269542ec026e65c1d024
Destinyrich <itsdestinyrich@gmail.com>
2023-04-20 02:49:37 -0700

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,12 @@
 #include "funtion_pointers.h"
 #include <studio.h>
-
 /**
  * print_name - print name using pointer to function
  * @name: string to add
  * @f: pointer to function
  * Return: nothing
 */
-void print_name(char *name, void(*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
