8613573372d8b9fdcc8285fe1c60142dcb1a57bf
Destinyrich <itsdestinyrich@gmail.com>
2023-04-20 02:55:11 -0700

    name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
-#include "funtion_pointers.h"
-#include <studio.h>
+#include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - print name using pointer to function
  * @name: string to add
