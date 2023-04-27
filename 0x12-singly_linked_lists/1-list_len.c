85e48dc2df888ee5a6aee7d3763f7f20660fcae0
ogbanefe Destiny <itsdestinyrich@gmail.com>
2023-04-27 15:55:30 -0700

    second

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,8 +2,8 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list 
- * @h: pointer to the list_t list 
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
  * Return:number of elemens in h
  */
@@ -12,7 +12,7 @@ size_t list_len(const list_t *h)
 	size_t n = 0;
 
 	while (h)
-	{ 
+	{
 		n++;
 		h = h->next;
 	}
